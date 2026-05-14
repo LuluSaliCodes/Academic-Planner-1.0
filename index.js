9fb3c26e6db7d32caeedf6a0965d53cc42608d85
The-Silent-Orchid <107502113+LuluSaliCodes@users.noreply.github.com>
2026-05-14 13:36:18 +0200

    Trying to Display list

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const form = document.getElementById("modInput");
 const inputBox = document.getElementById("input-el");
-
 const savedMods = [];
 
 form.addEventListener("submit", (event) => {
@@ -15,5 +14,21 @@ form.addEventListener("submit", (event) => {
     // Save the input
     savedMods.push(value);
     console.log(savedMods);
+
+    localStorage.setItem(
+        "myMods",
+        JSON.stringify(savedMods)
+    );
+
     inputBox.value = "";
 });
+
+const outputMods = document.getElementById("mod-list");
+const storedMods = localStorage.getItem("myMods");
+const arrMods = JSON.parse(storedMods) || [];
+
+for (const item of arrMods) {
+    const m = document.createElement("li");
+    m.textContent = item;
+    outputMods.appendChild(m);
+}
