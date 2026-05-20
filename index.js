8a108532288382437e71ffba2674bc5a46469f27
The-Silent-Orchid <107502113+LuluSaliCodes@users.noreply.github.com>
2026-05-20 20:25:31 +0200

    Added Module Blocks on Dashboard

--- index.js
+++ index.js
@@ -31,13 +31,25 @@ if (outputMods) {
     const storedMods = localStorage.getItem("myMods");
     const arrMods = JSON.parse(storedMods) || [];
 
-    for (let i = 0; i < arrMods.length; i++) {
-        console.log(arrMods[i]);
-    }
-
     for (const item of arrMods) {
         const m = document.createElement("p");
         m.textContent = item;
         outputMods.appendChild(m);
+    }
+}
+
+const modBlocks = document.getElementById("block-container");
+
+if (modBlocks) {
+    const temp = localStorage.getItem("myMods");
+    const mods = JSON.parse(temp) || [];
+
+    for (const el of mods) {
+        const mod_el = document.createElement("div");
+
+        mod_el.classList.add("dashboard-block");
+        mod_el.id = 'block-${el}';
+        mod_el.textContent = el;
+        modBlocks.appendChild(mod_el);
     }
 }
